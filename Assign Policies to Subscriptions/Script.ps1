6559951d8ea92db583e19d22248fe050696f0ddf
Richard Davies <richard@monsterpuss.co.uk>
2018-01-30 12:26:35 +0000

    tweaked

--- Assign Policies to Subscriptions/Script.ps1
+++ Assign Policies to Subscriptions/Script.ps1
@@ -6,18 +6,18 @@ $Selectedpolicies = @()
 $Selectedsubscriptions = @() 
  
 # Login-AzureRmAccount
-        $SelectedSubscriptions = Get-AzureRmSubscription | Select "$($SubNameVar)", "$($SubIdVar)" | Out-GridView -Title "Select Subscriptions (Ctrl/Shift click for multiples)" -PassThru  
+        $SelectedSubscriptions = Get-AzureRmSubscription | Select Name, Id | Out-GridView -Title "Select Subscriptions (Ctrl/Shift click for multiples)" -PassThru  
 		$polname = 
 		$poldef =
 		$displayname =
 		$SubscriptionId=
 		$Policies = @() 
         $Policies = Get-AzureRmPolicydefinition  | select name -ExpandProperty Properties | Sort-Object -Property @{Expression = {$_.IsCustom}; Ascending = $false}, Name | Out-GridView -title "Select Policies  (Ctrl/Shift click for multiples)" -PassThru 
-        Select-AzureRmSubscription -SubscriptionId $SelectedSubscriptions[0].$SubIdVar 
+        Select-AzureRmSubscription -SubscriptionId $SelectedSubscriptions[0].Id
  
         foreach ($Sub in $SelectedSubscriptions) { 
             foreach ($Policy in $Policies) { 
-				$subscriptionPath = "/subscriptions/" + $Sub.SubscriptionId
+				$subscriptionPath = "/subscriptions/" + $Sub.Id
 				$polname = $Policy.Name	
 				$poldef =  "/providers/Microsoft.Authorization/policyDefinitions/$Polname"
 				$displayname = $policy.displayName
